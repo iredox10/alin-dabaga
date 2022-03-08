3a053112237d38ed644d83e0ef3000a488971213
idris adam <idreesadam200@gmail.com>
2022-03-08 02:11:27 +0000

    added the add customer model

---
+++ model/customersModel.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+
+const customersSchema = mongoose.Schema({
+    fullName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    phoneNumber: {
+        type: String,
+        required: true
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    
+})
+
+module.exports = mongoose.model('customer', customersSchema)
