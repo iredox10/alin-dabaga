196ffe07287fe50c1da5572b69b274b0466efab0
idris adam <idreesadam200@gmail.com>
2022-03-17 22:12:50 +0000

    fix route problems

--- routes/routes.js
+++ routes/routes.js
@@ -2,19 +2,11 @@ const route = require('express').Router();
 const controller = require('../controller/controller')
 
 
-route.get('/',controller.home)
+route.get('/', controller.home)
+route.get('/:id',controller.customerDetail)
 route.post('/add-customer', controller.addCustomer)
 route.post('/add-product/:id', controller.add_product)
-
-// route.get('/login', controller.logIn)
-// route.post('/login', controller.post)
 route.delete('/delete-customer/:id', controller.delete_customer)
-route.get('/:id', controller.customerDetail)
-
-// route.get('/customer-detail/:id', controller.customerDetail)
-// route.delete('/delete-product/:id', controller.delete_product)
-
-
 
 
 module.exports = route
--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose');
 const ejs = require('ejs')
 const routes = require('./routes/routes')
 const methodOverride = require('method-override')
+const cookieParser = require('cookie-parser')
+
 mongoose.connect('mongodb://localhost/hassan-shop')
 .then((res) => {
     console.log('connected to mongodb ')
@@ -12,11 +14,15 @@ mongoose.connect('mongodb://localhost/hassan-shop')
 });
 
 
-app.set('view engine', 'ejs')
 app.use(express.urlencoded({extended:false}))
 app.use(express.static('public'))
+app.set('view engine', 'ejs')
 app.use(methodOverride('_method'))
+app.use(cookieParser())
 app.use(routes)
 
-
+app.get('/cookie', (req, res) => {
+    res.cookie('newCookie', true)
+    res.send('cookie sended')
+})
 app.listen(3000)
